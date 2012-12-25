d2fb826063dcc51e7db196593dbb7eb1821bccc3
Zoltan P <zoltan.padrah@users.sf.net>
2012-12-25 20:42:04 +0100

    remove debug messages

--- whitespaceterminator.py
+++ whitespaceterminator.py
@@ -33,29 +33,19 @@ class WhiteSpaceTerminator(GObject.Object, Gedit.WindowActivatable):
 
         if self._are_trailing_lines_stripped:
             if self._is_last_empty_line_kept:
-                print "removing trailing lines, keeping last one"
-                # processed_lines = document.props.text.splitlines()
-                # if there is a new line in the document, then
-                # add it as the last line to be procesed
+                # print "removing trailing lines, keeping last one"
                 lines = document.props.text.splitlines()
-                print "lines:"
-                print lines
-
                 while (len(lines) > 1) and ((lines[-2].isspace()) or (len(lines[-2]) == 0)):
                     lines.pop()
                 processed_lines = lines
             else:
-                print "removing trailing lines, not keeping last one"
+                # print "removing trailing lines, not keeping last one"
                 lines = document.props.text.splitlines()
-                print lines
                 while (len(lines) > 0) and ((lines[-1].isspace() or (len(lines[-1]) == 0))):
                     lines.pop()
                 processed_lines = lines
         else:
             processed_lines = document.props.text.splitlines()
-
-        print "processed lines:"
-        print processed_lines
 
         strip_start = document.get_end_iter()
         for i, text in enumerate(processed_lines):
@@ -63,10 +53,6 @@ class WhiteSpaceTerminator(GObject.Object, Gedit.WindowActivatable):
             strip_stop.forward_to_line_end()
             strip_start = strip_stop.copy()
             strip_start.backward_chars(len(text) - len(text.rstrip()))
-            print "processing line ", i, "text: ", text
-            # print "deleting: ", document.get_text(strip_start, strip_stop, 0)
-            print "line until strip start:"
-            print ("'%s'" % (document.get_text(document.get_iter_at_line(i), strip_start, 0)))
             document.delete(strip_start, strip_stop)
         document.delete(strip_start, document.get_end_iter())
 
